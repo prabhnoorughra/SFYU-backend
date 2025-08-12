26e3c907d9782aa0c45ad2b515f148938d1aaade
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-12 14:10:04 -0400

    Create Sample Seed Script for 2 Mock Users

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,26 @@
 const prisma = require("./prisma");
+const { Role } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
 
 
 async function main() {
   // ... you will write your Prisma Client queries here
+  await prisma.user.deleteMany();
+  const hashedPassword = await bcrypt.hash(process.env.PASSWORD, 10);
+  await prisma.user.create({
+    data: {
+      role: Role.ADMIN,
+      username: "psughra@uwaterloo.ca",
+      password: hashedPassword,
+    }
+  });
+  await prisma.user.create({
+    data: {
+      role: Role.USER,
+      username: "prabhnoor.ughra@gmail.com",
+      password: hashedPassword,
+    }
+  });
   const users = await prisma.user.findMany();
   console.log(users);
 }
