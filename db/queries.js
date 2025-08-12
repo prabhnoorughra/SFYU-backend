57224b12c8fe0562148ccf4164b8aa633101f0f9
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-12 14:10:38 -0400

    Implement Queries to Create and Search for Applicants and Users

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,4 @@
+const { get } = require("../routes/indexRouter");
 const prisma = require("./prisma");
 
 
@@ -19,7 +20,76 @@ async function insertUser(username, password) {
     });
     return user;
 };
+
+
+async function insertApplication(email, studentId, firstName, lastName, emailConsent, 
+    program, studyYear, fullName) {
+    const applicant = await prisma.application.create({
+        data: {
+            email,
+            studentId,
+            firstName,
+            lastName,
+            emailConsent,
+            program,
+            studyYear,
+            fullName,
+        }
+    });
+    return applicant;
+};
+
+async function findByEmail(email) {
+   const applicant = await prisma.application.findUnique({
+    where: {
+        email: email,
+    }
+   });
+    return applicant;
+}
+
+async function findByStudentId(studentId) {
+   const applicant = await prisma.application.findUnique({
+    where: {
+        studentId: studentId,
+    }
+   });
+    return applicant;
+}
+
+async function getAllApplications() {
+    const apps = await prisma.application.findMany();
+    return apps;
+}
+
+async function searchApplications(query) {
+    const results = await prisma.application.findMany({
+        where: {
+            OR: [
+                { email:     { contains: query, mode: 'insensitive' } },
+                { studentId: { contains: query } },
+                { firstName: { contains: query, mode: 'insensitive' } },
+                { lastName:  { contains: query, mode: 'insensitive' } },
+                { fullName:  { contains: query, mode: 'insensitive' } },
+            ],
+        }
+    });
+    return results;
+}
+
+async function getApplicantCount() {
+    const count = await prisma.application.count();
+    return count;
+}
+
+
 module.exports = {
     findUser,
     insertUser,
+    findByEmail,
+    findByStudentId,
+    getAllApplications,
+    insertApplication,
+    getApplicantCount,
+    searchApplications,
 }
